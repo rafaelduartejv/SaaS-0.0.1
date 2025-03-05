9e739c3d91978129aec0a56ccdf9eef6d9029dc8
Rafael Santos <109754987+rafaelduartejv@users.noreply.github.com>
2025-03-05 15:26:11 -0300

    feat: adicionado email

--- src/main/java/com/example/auth/domain/user/RegisterDTO.java
+++ src/main/java/com/example/auth/domain/user/RegisterDTO.java
@@ -1,4 +1,4 @@
 package com.example.auth.domain.user;
 
-public record RegisterDTO(String login, String password, UserRole role) {
+public record RegisterDTO(String login, String password, String email, UserRole role) {
 }
--- src/main/java/com/example/auth/domain/user/User.java
+++ src/main/java/com/example/auth/domain/user/User.java
@@ -24,11 +24,13 @@ public class User implements UserDetails {
     private String id;
     private String login;
     private String password;
+    private String email;
     private UserRole role;
 
     public User(String login, String password, UserRole role){
         this.login = login;
         this.password = password;
+        this.email = email;
         this.role = role;
     }
 
